54c61d5d7b971ef50ccac9e8b7e2574459174b2f
rooshert <visheyn@gmail.com>
2022-09-13 13:38:57 +0300

    admin panel update

--- sessioncart/orders/admin.py
+++ sessioncart/orders/admin.py
@@ -3,8 +3,17 @@ import datetime
 from django.http import HttpResponse
 from django.contrib import admin
 
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+
 from .models import Order, OrderItem
 import ipdb
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
 
 def export_to_csv(modeladmin, request, queryset):
     ipdb.set_trace()
@@ -29,6 +38,7 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 
 export_to_csv.short_description = 'Export to CSV'
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
